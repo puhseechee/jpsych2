50db98fbd8527fe2382505f78c17db19cd428754
Jordan Gunn <jgunn@princeton.edu>
2017-04-26 04:31:00 -0400

    Update jspsych-photo-upload.js

--- jspsych-5.0/plugins/jspsych-photo-upload.js
+++ jspsych-5.0/plugins/jspsych-photo-upload.js
@@ -87,6 +87,13 @@ jsPsych.plugins['photo-upload'] = (function(){
     
       // Enable delete button
       delete_photo_btn.classList.remove("disabled");
+      
+      // show "Next" button
+      display_element.append($('<button>', {
+        'id': 'next1',
+        'class': 'button1',
+        'html': 'Next'
+      }));
     
       // Pause video playback of stream.
       video.pause();
@@ -164,99 +171,92 @@ jsPsych.plugins['photo-upload'] = (function(){
       error_message.classList.remove("visible");
     }
     
-    // end with a button to submit a result; it needs to hide the video and prompt and the button, producing new stuff
-    display_element.append($('<button>', {
-      'id': 'next1',
-      'class': 'button1',
-      'html': 'Next'
-    }));
-    
     // part of experiment after first click of next button
     $("#next1").click(function() {
-        // hide prompt
-        prompthtml = document.getElementsByClassName('prompt')[0];
-        prompthtml.style.display = 'none';
-        
-        // hide photo
-        app = document.getElementsByClassName('container')[0];
-        app.style.display = 'none';
+      // hide prompt
+      prompthtml = document.getElementsByClassName('prompt')[0];
+      prompthtml.style.display = 'none';
+      
+      // hide photo
+      app = document.getElementsByClassName('container')[0];
+      app.style.display = 'none';
+      
+      // hide button
+      button = document.getElementsByClassName('button1')[0];
+      button.style.display = 'none';
+
+      // then follow up instructions (dependent on condition)
+      if (trial.condition == 1) {
+          display_element.append('<div class="followup">Thank you for uploading a photo of yourself smiling!&nbsp;<div><br></div><div>Next, we would like you to view your photo.</div><div>Your photo will be posted on the next page, and viewed by you.&nbsp;</div><div><br></div><div>Please click "Next" to continue.&nbsp;</div></div>');
+      } else {
+          display_element.append('<div class="followup">Thank you for uploading a photo of yourself smiling!<br> <br> Next, we would like to share your photo with other MTurk workers who are standing by and participating in a different study.&nbsp;<div>In a moment, your photo will be posted on our server, and these other MTurk workers will view your photo. On the next page, you will see your photo post, along with how many MTurk workers are viewing it.&nbsp;<div><div><br></div><div>Please click "Next" to continue.&nbsp;</div></div></div></div>');
+      }
+      
+      // a new button
+      display_element.append($('<button>', {
+          'id': 'next2',
+          'class': 'button2',
+          'html': 'Next'
+      }));
+      
+      // part of experiment after second click of next button
+      $("#next2").click(function() {
+        // hide followup instructions
+        followup = document.getElementsByClassName('followup')[0];
+        followup.style.display = 'none';
         
         // hide button
-        button = document.getElementsByClassName('button1')[0];
+        button = document.getElementsByClassName('button2')[0];
         button.style.display = 'none';
-
-        // then follow up instructions (dependent on condition)
+        
+        // then server loading page
         if (trial.condition == 1) {
-            display_element.append('<div class="followup">Thank you for uploading a photo of yourself smiling!&nbsp;<div><br></div><div>Next, we would like you to view your photo.</div><div>Your photo will be posted on the next page, and viewed by you.&nbsp;</div><div><br></div><div>Please click "Next" to continue.&nbsp;</div></div>');
+            display_element.append('<div class="loading"><div style="text-align: center;">Please wait while the server loads.<br> <br> <img src="https://raw.githubusercontent.com/puhseechee/jpsych2/jpsychadd/jspsych-5.0/loading.gif" style="width: 144px; height: 144px;"></div></div>');
         } else {
-            display_element.append('<div class="followup">Thank you for uploading a photo of yourself smiling!<br> <br> Next, we would like to share your photo with other MTurk workers who are standing by and participating in a different study.&nbsp;<div>In a moment, your photo will be posted on our server, and these other MTurk workers will view your photo. On the next page, you will see your photo post, along with how many MTurk workers are viewing it.&nbsp;<div><div><br></div><div>Please click "Next" to continue.&nbsp;</div></div></div></div>');
+            display_element.append('<div class="loading"><div style="text-align: center;">Please wait while we upload your message on our server.<br> <br> <img src="https://raw.githubusercontent.com/puhseechee/jpsych2/jpsychadd/jspsych-5.0/loading.gif" style="width: 144px; height: 144px;"></div></div>');
         }
         
-        // a new button
-        display_element.append($('<button>', {
-            'id': 'next2',
-            'class': 'button2',
+        // after 3 seconds, move on to the next step
+        // for now, just end the experiment
+        setTimeout(function(){
+          // hide the other stuff
+          loading = document.getElementsByClassName('loading')[0];
+          loading.style.display = 'none';
+          
+          if (trial.condition == 1) {
+              // unhide photo, but do hide control buttons
+              $("container").show();
+              // add final text
+              display_element.append('<div class="final" style="text-align: center;">Here is your photo. Please view it.<br> The "Next" button will appear in a few moments.&nbsp;</div><div style="text-align: center;">Please click it to continue</div>');
+          } else if (trial.condition == 2) {
+              // show everything that was in the other one
+              display_element.append('<div style="text-align: center;">Please wait while other participants view and read your message.&nbsp; &nbsp;</div><div style="text-align: center;"><br><b> Participants currently viewing your message:&nbsp;</b></div> <div style="text-align: center;">&nbsp;</div> <div style="text-align: center;"> <video autoplay="" class="qmedia" height="240" peload="auto" width="360"><source src="https://cdn.rawgit.com/puhseechee/jpsych2/jpsychadd/jspsych-5.0/Alone_30sec.mp4" type="video/mp4"> <embed align="middle" autoplay="true" bgcolor="white" class="qmedia" height="240" pluginspage="http://www.apple.com/quicktime/download/" src="https://cdn.rawgit.com/puhseechee/jpsych2/jpsychadd/jspsych-5.0/3user_15sec.mp4" type="video/quicktime" width="360"></video>&nbsp;</div><div style="text-align: center;"><br> The "Next" button will appear in a few moments.&nbsp;</div> <div style="text-align: center;">Please click it to continue.</div>');
+          } else {
+              // same for final condition
+              display_element.append('<div style="text-align: center;"> <div style="text-align: center;">Please wait while other participants view and read your message.&nbsp; &nbsp;</div><div style="text-align: center;"><br><b> Participants currently viewing your message:&nbsp;</b></div> <div style="text-align: center;">&nbsp;</div> <div style="text-align: center;"> <video autoplay="" class="qmedia" height="240" peload="auto" width="360"><source src="https://cdn.rawgit.com/puhseechee/jpsych2/jpsychadd/jspsych-5.0/3user_15sec.mp4" type="video/mp4"><embed align="middle" autoplay="true" bgcolor="white" class="qmedia" height="240" pluginspage="http://www.apple.com/quicktime/download/" src="https://cdn.rawgit.com/puhseechee/jpsych2/jpsychadd/jspsych-5.0/3user_15sec2.mp4" type="video/quicktime" width="360"></video>&nbsp;</div><div style="text-align: center;"><br> <div style="text-align: center;"> The "Next" button will appear in a few moments.&nbsp;</div> <div style="text-align: center;">Please click it to continue.</div> </div></div>');
+          }
+          
+          // no matter what, we want a final "Next Button" to finish the trial with
+          display_element.append($('<button>', {
+            'id': 'nextlast',
+            'class': 'buttonlast',
             'html': 'Next'
-        }));
-        
-        // part of experiment after second click of next button
-        $("#next2").click(function() {
-            // hide followup instructions
-            followup = document.getElementsByClassName('followup')[0];
-            followup.style.display = 'none';
+          }));
+          
+          $("#nextlast").click(function() {
+            display_element.html(''); // clear the display
             
-            // hide button
-            button = document.getElementsByClassName('button2')[0];
-            button.style.display = 'none';
+            // data saving
+            var trialdata = {
+              prompt: trial.prompt,
+              condition: trial.condition,
+            };
             
-            // then server loading page
-            if (trial.condition == 1) {
-                display_element.append('<div class="loading"><div style="text-align: center;">Please wait while the server loads.<br> <br> <img src="https://raw.githubusercontent.com/puhseechee/jpsych2/jpsychadd/jspsych-5.0/loading.gif" style="width: 144px; height: 144px;"></div></div>');
-            } else {
-                display_element.append('<div class="loading"><div style="text-align: center;">Please wait while we upload your message on our server.<br> <br> <img src="https://raw.githubusercontent.com/puhseechee/jpsych2/jpsychadd/jspsych-5.0/loading.gif" style="width: 144px; height: 144px;"></div></div>');
-            }
-            
-            // after 3 seconds, move on to the next step
-            // for now, just end the experiment
-            setTimeout(function(){
-              // hide the other stuff
-              loading = document.getElementsByClassName('loading')[0];
-              loading.style.display = 'none';
-              
-              if (trial.condition == 1) {
-                  // unhide photo, but do hide control buttons
-                  $("container").show();
-                  // add final text
-                  display_element.append('<div class="final" style="text-align: center;">Here is your photo. Please view it.<br> The "Next" button will appear in a few moments.&nbsp;</div><div style="text-align: center;">Please click it to continue</div>');
-              } else if (trial.condition == 2) {
-                  // show everything that was in the other one
-                  display_element.append('<div style="text-align: center;">Please wait while other participants view and read your message.&nbsp; &nbsp;</div><div style="text-align: center;"><br><b> Participants currently viewing your message:&nbsp;</b></div> <div style="text-align: center;">&nbsp;</div> <div style="text-align: center;"> <video autoplay="" class="qmedia" height="240" peload="auto" width="360"><source src="https://cdn.rawgit.com/puhseechee/jpsych2/jpsychadd/jspsych-5.0/Alone_30sec.mp4" type="video/mp4"> <embed align="middle" autoplay="true" bgcolor="white" class="qmedia" height="240" pluginspage="http://www.apple.com/quicktime/download/" src="https://cdn.rawgit.com/puhseechee/jpsych2/jpsychadd/jspsych-5.0/3user_15sec.mp4" type="video/quicktime" width="360"></video>&nbsp;</div><div style="text-align: center;"><br> The "Next" button will appear in a few moments.&nbsp;</div> <div style="text-align: center;">Please click it to continue.</div>');
-              } else {
-                  // same for final condition
-                  display_element.append('<div style="text-align: center;"> <div style="text-align: center;">Please wait while other participants view and read your message.&nbsp; &nbsp;</div><div style="text-align: center;"><br><b> Participants currently viewing your message:&nbsp;</b></div> <div style="text-align: center;">&nbsp;</div> <div style="text-align: center;"> <video autoplay="" class="qmedia" height="240" peload="auto" width="360"><source src="https://cdn.rawgit.com/puhseechee/jpsych2/jpsychadd/jspsych-5.0/3user_15sec.mp4" type="video/mp4"><embed align="middle" autoplay="true" bgcolor="white" class="qmedia" height="240" pluginspage="http://www.apple.com/quicktime/download/" src="https://cdn.rawgit.com/puhseechee/jpsych2/jpsychadd/jspsych-5.0/3user_15sec2.mp4" type="video/quicktime" width="360"></video>&nbsp;</div><div style="text-align: center;"><br> <div style="text-align: center;"> The "Next" button will appear in a few moments.&nbsp;</div> <div style="text-align: center;">Please click it to continue.</div> </div></div>');
-              }
-              
-              // no matter what, we want a final "Next Button" to finish the trial with
-              display_element.append($('<button>', {
-                'id': 'nextlast',
-                'class': 'buttonlast',
-                'html': 'Next'
-              }));
-              
-              $("#nextlast").click(function() {
-                display_element.html(''); // clear the display
-                
-                // data saving
-                var trialdata = {
-                  prompt: trial.prompt,
-                  condition: trial.condition,
-                };
-                
-                jsPsych.finishTrial(trialdata); // end
-              });
-            }, 3000);
-            });
+            jsPsych.finishTrial(trialdata); // end
+          });
+        }, 3000);
         });
+      });
     };
     
       return plugin;
