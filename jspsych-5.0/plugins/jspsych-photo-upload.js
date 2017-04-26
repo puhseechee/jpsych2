110634d952d0cdf2944547dc3490f3d3b5d9066b
Jordan Gunn <jgunn@princeton.edu>
2017-04-26 05:10:14 -0400

    Update jspsych-photo-upload.js

--- jspsych-5.0/plugins/jspsych-photo-upload.js
+++ jspsych-5.0/plugins/jspsych-photo-upload.js
@@ -211,7 +211,6 @@ jsPsych.plugins['photo-upload'] = (function(){
         // hide button
         button = document.getElementsByClassName('button2')[0];
         button.style.display = 'none';
-        controls.classList.remove("visible");
         
         // then server loading page
         if (trial.condition == 1) {
@@ -230,6 +229,7 @@ jsPsych.plugins['photo-upload'] = (function(){
           if (trial.condition == 1) {
               // unhide photo, but do hide control buttons
               app.style.display = 'block';
+              controls.classList.remove("visible");
               // add final text
               display_element.append('<div class="final" style="text-align: center;">Here is your photo. Please view it.<br> The "Next" button will appear in a few moments.&nbsp;</div><div style="text-align: center;">Please click it to continue</div>');
           } else if (trial.condition == 2) {
